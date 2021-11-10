1c2994c396cba4b4cb0570744d6298e3c25ea662
Tobias Gerschner <tobias.gerschner@cultureamp.com>
2021-11-11 10:37:56 +1300

    fix match

--- lib/steps.bash
+++ lib/steps.bash
@@ -83,7 +83,7 @@ function load_env_file() {
     return
   fi
 
-  local vars ;  vars="$( awk -F '=' '$1 ~ /^([a-zA-Z]).*=/ {print $1}' "${env_file}" )"
+  local vars ;  vars="$( awk -F '=' '$0 ~ /^([a-zA-Z]).*=/ {print $1}' "${env_file}" )"
   # shellcheck disable=SC1090
   source "$env_file"
   export vars
